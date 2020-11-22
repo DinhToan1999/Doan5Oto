b8da6ae2d6a646e96374e85e30633083a70b7721
Your Name <vudinhtoan1999@gmail.com>
2020-11-22 21:30:24 +0700

    main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,22 +11,48 @@ import 'rxjs/add/operator/takeUntil';
 })
 export class MainComponent extends BaseComponent implements OnInit {
   list_item:any;
+  new:any;
+  km:any;
   constructor(injector: Injector) { 
     super(injector);
   }
-
-
 ngOnInit(): void {
+  this.getproall();
+  this.GetProdNew();
+  this.GetProdKm();
+}
+getproall(){
+Observable.combineLatest(
+  
+  this._api.get('/api/Product/get-all'),
+).takeUntil(this.unsubscribe).subscribe(res => {
+  this.list_item = res[0];
+  setTimeout(() => {
+    this.loadScripts();
+  });
+}, err => { });
+}
+GetProdNew(){
   Observable.combineLatest(
-    this._api.get('/api/Product/get-all'),
+  
+    this._api.get('/api/Product/get-prod-new'),
   ).takeUntil(this.unsubscribe).subscribe(res => {
-    this.list_item = res[0];
+    this.new = res[0];
     setTimeout(() => {
       this.loadScripts();
     });
   }, err => { });
 }
-
-
+GetProdKm(){
+  Observable.combineLatest(
+  
+    this._api.get('/api/Product/get-prod-km'),
+  ).takeUntil(this.unsubscribe).subscribe(res => {
+    this.km = res[0];
+    setTimeout(() => {
+      this.loadScripts();
+    });
+  }, err => { });
+}
 }
 
